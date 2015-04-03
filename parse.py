4841ab58b34e23c5360dfc572adcf1e11d5f467b
Tara L Andrews <tla@mit.edu>
2015-04-03 22:27:22 +0200

    allow configurable glyph list; ensure namespace resolution of returned TEI; sanitize function names

--- parse.py
+++ parse.py
@@ -4,16 +4,41 @@ __author__ = 'tla'
 import json
 import re
 import sys
+from io import BytesIO
 from lxml import etree
 from warnings import warn
 
-# The JSON file is a Shared Canvas specification. It has a series of sequences
-# (should be 1 sequence), and each sequence has a set of canvases, each of
-# which is a page.
+# Our default (and example) list of special characters that might occur as
+# glyph (<g/>) elements. A true list should be passed to the from_sc call.
+# The key is the normalized form; the tuple is (xml:id, description).
+_armenian_glyphs = {
+    'աշխարհ': ('asxarh', 'ARMENIAN ASHXARH SYMBOL'),
+    'ամենայն': ('amenayn', 'ARMENIAN AMENAYN SYMBOL'),
+    'որպէս': ('orpes', 'ARMENIAN ORPES SYMBOL'),
+    'երկիր': ('erkir', 'ARMENIAN ERKIR SYMBOL'),
+    'երկին': ('erkin', 'ARMENIAN ERKIN SYMBOL'),
+    'ընդ': ('und', 'ARMENIAN END SYMBOL'),
+    'ըստ': ('ust', 'ARMENIAN EST SYMBOL'),
+    'պտ': ('ptlig', 'ARMENIAN PEH-TIWN LIGATURE'),
+    'թե': ('techlig', 'ARMENIAN TO-ECH LIGATURE'),
+    'թի': ('tinilig', 'ARMENIAN TO-INI LIGATURE'),
+    'թէ': ('tehlig', 'ARMENIAN TO-EH LIGATURE'),
+    'էս': ('eslig', 'ARMENIAN EH-SEH LIGATURE'),
+    'ես': ('echslig', 'ARMENIAN ECH-SEH LIGATURE'),
+    'յր': ('yrlig', 'ARMENIAN YI-REH LIGATURE'),
+    'զմ': ('zmlig', 'ARMENIAN ZA-MEN LIGATURE'),
+    'թգ': ('tglig', 'ARMENIAN TO-GIM LIGATURE'),
+    'ա': ('avar', 'ARMENIAN AYB VARIANT'),
+    'հ': ('hvar', 'ARMENIAN HO VARIANT'),
+    'յ': ('yabove', 'ARMENIAN YI SUPERSCRIPT VARIANT')
+}
 
 
-def from_sc(jsondata):
-    """Extract the textual transcription from the Shared Canvas data."""
+def from_sc(jsondata, special_chars=_armenian_glyphs):
+    """Extract the textual transcription from a JSON file, probably exported
+    from T-PEN according to a Shared Canvas specification. It has a series of
+    sequences (should be 1 sequence), and each sequence has a set of canvases,
+    each of which is a page."""
     if len(jsondata['sequences']) > 1:
         warn("Your data has more than one sequence. Check to see what's going on.", UserWarning)
     pages = jsondata['sequences'][0]['canvases']
@@ -69,61 +94,64 @@ def from_sc(jsondata):
     # Report how many columns per page.
     for n in sorted(columns.keys()):
         print("%d columns for pages %s\n" % (n, " ".join(columns[n])), file=sys.stderr)
-    return "<body><ab>%s</ab></body>" % xmlstring
+    return _xmlify("<body><ab>%s</ab></body>" % xmlstring, special_chars=special_chars)
 
 
-def xmlify(txdata):
+def _xmlify(txdata, special_chars=None):
     """Take the extracted XML structure of from_sc and make sure it is
     well-formed. Also fix any shortcuts, e.g. for the glyph tags."""
     try:
         content = etree.fromstring(txdata)
     except etree.XMLSyntaxError as e:
-        print("Parsing error in the JSON: %s" % e.msg)
-        print("Full string was %s" % txdata)
+        print("Parsing error in the JSON: %s" % e.msg, file=sys.stderr)
+        print("Full string was %s" % txdata, file=sys.stderr)
         return
 
     # Now fix the glyph references.
     glyphs_seen = {}
-    # LATER get this hard-coded list into a settings file.
-    glyph_correction = {
-        'the': 'թե',
-        'thE': 'թէ',
-        'und': 'ընդ',
-        'thi': 'թի',
-        'asxarh': 'աշխարհ',
-        'pt': 'պտ',
-        'yr': 'յր',
-        'orpes': 'որպէս',
-    }
-    for glyph in content.xpath('//g'):
-        # Find the characters that we have glyph-marked. It could have been done
-        # in a couple of different ways.
-        gchars = ''
-        gtext_explicit = ''
-        if glyph.get('ref'):
-            gchars = glyph.get('ref')
-            if gchars.find('#') == 0:
-                gchars = gchars[1:]
-        if glyph.text:
-            if gchars == '':
-                gchars = glyph.text
-            else:
-                gtext_explicit = True
-        if gchars in glyph_correction:
-            gchars = glyph_correction[gchars]
-        # Now figure out what the reference is for this glyph. Make the
-        # XML element if necessary.
-        if gchars not in glyphs_seen:
-            try:
-                glyphs_seen[gchars] = get_glyph(gchars)
-            except ValueError as e:
-                print("In g element %s:" % etree.tostring(glyph, encoding='unicode', with_tail=False), file=sys.stderr)
-                raise e
-        gref = '#%s' % glyphs_seen[gchars].get('{http://www.w3.org/XML/1998/namespace}id')
-        # Finally, fix the 'g' element here so that it is canonical.
-        glyph.set('ref', gref)
-        if not gtext_explicit:
-            glyph.text = gchars
+    if special_chars is not None:
+        # LATER get this hard-coded list into a settings file. Or better yet, correct
+        # the transcriptions.
+        glyph_correction = {
+            'the': 'թե',
+            'thE': 'թէ',
+            'und': 'ընդ',
+            'thi': 'թի',
+            'asxarh': 'աշխարհ',
+            'pt': 'պտ',
+            'yr': 'յր',
+            'orpes': 'որպէս',
+        }
+        for glyph in content.xpath('//g'):
+            # Find the characters that we have glyph-marked. It could have been done
+            # in a couple of different ways.
+            gchars = ''
+            gtext_explicit = ''
+            if glyph.get('ref'):
+                gchars = glyph.get('ref')
+                if gchars.find('#') == 0:
+                    gchars = gchars[1:]
+            if glyph.text:
+                if gchars == '':
+                    gchars = glyph.text
+                else:
+                    gtext_explicit = True
+            if gchars in glyph_correction:
+                gchars = glyph_correction[gchars]
+            # Now figure out what the reference is for this glyph. Make the
+            # XML element if necessary.
+            if gchars not in glyphs_seen:
+                try:
+                    glyphs_seen[gchars] = _get_glyph(gchars, special_chars)
+                except ValueError as e:
+                    print("In g element %s:" % etree.tostring(glyph, encoding='unicode', with_tail=False),
+                          file=sys.stderr)
+                    raise e
+            gref = '#%s' % glyphs_seen[gchars].get('{http://www.w3.org/XML/1998/namespace}id')
+            # Finally, fix the 'g' element here so that it is canonical.
+            glyph.set('ref', gref)
+            if not gtext_explicit:
+                glyph.text = gchars
 
     for el in content.xpath('//corr'):
         el.tag = 'subst'
@@ -143,45 +171,24 @@ def xmlify(txdata):
             else:
                 el.set('cert', 'low')
 
-    return tei_wrap(content, sorted(glyphs_seen.values(),
-                    key=lambda x: x.get('{http://www.w3.org/XML/1998/namespace}id')))
+    return _tei_wrap(content, sorted(glyphs_seen.values(),
+                                     key=lambda x: x.get('{http://www.w3.org/XML/1998/namespace}id')))
 
 
-def get_glyph(gname):
+def _get_glyph(gname, special_chars):
     """Returns a TEI XML 'glyph' element for the given string."""
     # LATER get this hard-coded list into a settings file.
-    armenian_glyphs = {
-        'աշխարհ': ('asxarh', 'ARMENIAN ASHXARH SYMBOL'),
-        'ամենայն': ('amenayn', 'ARMENIAN AMENAYN SYMBOL'),
-        'որպէս': ('orpes', 'ARMENIAN ORPES SYMBOL'),
-        'երկիր': ('erkir', 'ARMENIAN ERKIR SYMBOL'),
-        'երկին': ('erkin', 'ARMENIAN ERKIN SYMBOL'),
-        'ընդ': ('und', 'ARMENIAN END SYMBOL'),
-        'ըստ': ('ust', 'ARMENIAN EST SYMBOL'),
-        'պտ': ('ptlig', 'ARMENIAN PEH-TIWN LIGATURE'),
-        'թե': ('techlig', 'ARMENIAN TO-ECH LIGATURE'),
-        'թի': ('tinilig', 'ARMENIAN TO-INI LIGATURE'),
-        'թէ': ('tehlig', 'ARMENIAN TO-EH LIGATURE'),
-        'էս': ('eslig', 'ARMENIAN EH-SEH LIGATURE'),
-        'ես': ('echslig', 'ARMENIAN ECH-SEH LIGATURE'),
-        'յր': ('yrlig', 'ARMENIAN YI-REH LIGATURE'),
-        'զմ': ('zmlig', 'ARMENIAN ZA-MEN LIGATURE'),
-        'թգ': ('tglig', 'ARMENIAN TO-GIM LIGATURE'),
-        'ա': ('avar', 'ARMENIAN AYB VARIANT'),
-        'հ': ('hvar', 'ARMENIAN HO VARIANT'),
-        'յ': ('yabove', 'ARMENIAN YI SUPERSCRIPT VARIANT')
-    }
-    if gname not in armenian_glyphs:
+    if gname not in special_chars:
         raise ValueError("Glyph %s not recognized" % gname)
 
     glyph_el = etree.Element('glyph')
-    glyph_el.set('{http://www.w3.org/XML/1998/namespace}id', '%s' % armenian_glyphs[gname][0])
-    etree.SubElement(glyph_el, 'glyphName').text = armenian_glyphs[gname][1]
+    glyph_el.set('{http://www.w3.org/XML/1998/namespace}id', '%s' % special_chars[gname][0])
+    etree.SubElement(glyph_el, 'glyphName').text = special_chars[gname][1]
     etree.SubElement(glyph_el, 'mapping').text = gname
     return glyph_el
 
 
-def tei_wrap(content, glyphs):
+def _tei_wrap(content, glyphs):
     """Wraps the content, and the glyphs that were found, into TEI XML format."""
     # First make the skeleton
     tei = etree.Element('TEI')
@@ -190,7 +197,8 @@ def tei_wrap(content, glyphs):
     etree.SubElement(etree.SubElement(file_desc, 'publicationStmt'), 'p').text = 'This is the publication statement'
     etree.SubElement(etree.SubElement(file_desc, 'sourceDesc'), 'p').text = 'This is the source description'
     # Then add the glyphs we used
-    etree.SubElement(etree.SubElement(tei.find('teiHeader'), 'encodingDesc'), 'charDecl').extend(glyphs)
+    if len(glyphs):
+        etree.SubElement(etree.SubElement(tei.find('teiHeader'), 'encodingDesc'), 'charDecl').extend(glyphs)
     # Then add the content.
     etree.SubElement(tei, 'text').append(content)
     # Finally, set the appropriate namespace and schema.
@@ -198,13 +206,15 @@ def tei_wrap(content, glyphs):
     tei_doc = etree.ElementTree(tei)
     schema = etree.ProcessingInstruction('xml-model', 'href="http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"')
     tei.addprevious(schema)
+    # Now that we've done this, serialize and re-parse the entire TEI doc
+    # so that the namespace functionality works.
+    tei_doc = etree.parse(BytesIO(etree.tostring(tei_doc)))
     return tei_doc
 
 
 if __name__ == '__main__':
-    with open(sys.argv[1]) as jfile:
+    with open(sys.argv[1], encoding='utf-8') as jfile:
         msdata = json.load(jfile)
-    parsed = from_sc(msdata)
-    xmltree = xmlify(parsed)
+    xmltree = from_sc(msdata)
     if xmltree is not None:
         sys.stdout.buffer.write(etree.tostring(xmltree, encoding='utf-8', pretty_print=True, xml_declaration=True))
